17178b62d43ba5f9869e1cb094f360f9f84f8d99
Liuchiyi <429531435@qq.com>
2018-07-28 21:16:29 +0800

    加入返回登陆链接

--- src/main/webapp/static/Myjs/login.js
+++ src/main/webapp/static/Myjs/login.js
@@ -2,17 +2,51 @@ $("#login").click(function () {
     $.ajax({
         type:'post',
         url:'../user/login',
+        async:false,
         data:{
             account:$("#account").val(),
             password:$("#password").val(),
         },
         success:function(result){
             if(result == "success"){
-                window.location.href="../views/motorSearch.html";
+                setTimeout(function(){
+                    window.location.href="../views/motorSearch.html";
+                },1);
+
             }else {
                 alertWindow("用户名或密码错误", "error");
             }
+        },
+        error:function () {
+            alertWindow("出错了", "error");
         }
     });
     
 })
+$("#registering").click(function () {
+    $.ajax({
+        type:'post',
+        url:'../user/register',
+        async:false,
+        data:{
+            account:$("#account").val(),
+            name:$("#name").val(),
+            email:$("#email").val(),
+            password:$("#password").val(),
+        },
+        success:function(result){
+            if(result == "success"){
+                alertWindow("注册成功", "success");
+                setTimeout(function(){
+                    window.location.href="index.html";
+                },1);
+            }else {
+                alert("注册失败");
+                alertWindow("用户名/账号/邮箱冲突", "error");
+            }
+        }
+    });
+
+})
+
+
